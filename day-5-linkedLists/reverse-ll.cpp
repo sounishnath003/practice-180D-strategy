08eb9e974a599e0133d77cb7ddb580cbe57e3087
Sounish Nath <flock.sinasini@gmail.com>
2020-06-30 23:18:57 +0530

    reverse ll

--- day-5-linkedLists/reverse-ll.cpp
+++ day-5-linkedLists/reverse-ll.cpp
@@ -8,10 +8,52 @@ using namespace std ;
 #define MOD = 1e9+7 ;
 const int N = 1e6+7 ;
 
+struct Node {
+    int data ;
+    Node *next ;
+    Node(int d) : data(d), next(NULL) {}
+};
+
+struct LinkedList {
+    Node *root ;
+    LinkedList() : root(NULL) {} 
+    void push(int data) {
+        Node *t = new Node(data);
+        t->next = root ;
+        root = t ;
+    }
+    void reverse() {
+        Node *cr = root ;
+        Node *prev, *next;
+        prev = next = NULL;
+        while(cr) {
+            next = cr->next ;
+            cr->next = prev ;
+            swap(prev, cr->next) ;
+        }
+        root = prev ;
+    }
+    void print(){
+        while(root){
+            cout << root->data << " " ;
+            root = root->next ;
+        }
+        cout << endl ;
+    }
+};
+
+
 int main(int argc, char const *argv[]){
     IOS ;
-   
     
+    LinkedList linkedlist ;
+    linkedlist.push(1);
+    linkedlist.push(2);
+    linkedlist.push(3);
+    linkedlist.push(5);
+    linkedlist.print();
+    linkedlist.reverse();
+    linkedlist.print() ;
 
     return 0;
 }
