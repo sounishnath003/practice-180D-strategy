fdb83905d8c4f4d3fa21b7b9151b01921f4fa595
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2020-10-13 13:35:25 +0530

    90 deg rotate - 2 sols
    1. Ineffieciant solution - O(N*M) space solution (BAD)
    2. Efficient solution - O(1) space solution (BEST)

--- product-based-checklist/Arrays/rotate-matrix-90-deg.cpp
+++ product-based-checklist/Arrays/rotate-matrix-90-deg.cpp
@@ -9,19 +9,9 @@
 
 using namespace std ;
 
-
-int main() {
-	srand(time(NULL)); cin.tie(0);
-	ios::sync_with_stdio(false);
-	
-	int m, n ;
-	cin >> m >> n;
-	vector<vector<int>> matrix(m, vector<int>(n));
-	for(int r = 0; r < m; r++){
-		for(int c = 0; c < n; c++){
-			cin >> matrix[r][c];
-		}
-	}
+/* efficient solution in O(1) ex */
+void f(vector<vector<int>> &matrix){
+	int m = matrix.size(), n = matrix[0].size();
 	int row = 0, col = 0, prev = 0, next = 0;
 	while(row < m && col < n){
 		if(row + 1 == m or col + 1 == n){
@@ -57,8 +47,30 @@ int main() {
 		}
 		col++;
 	}
+}
+
+
+int main() {
+	srand(time(NULL)); cin.tie(0);
+	ios::sync_with_stdio(false);
+	
+	int m, n ;
+	cin >> m >> n;
+	vector<vector<int>> matrix(m, vector<int>(n));
+	for(int r = 0; r < m; r++){
+		for(int c = 0; c < n; c++){
+			cin >> matrix[r][c];
+		}
+	}
+	/* ineffiecient solution for O(M*N) extra space */
+	vector<vector<int>> ans(m, vector<int>(n));
+	for(int row = 0; row < m; row++){
+		for(int col = 0; col < n; col++){
+			ans[row][col] = matrix[m - col - 1][row];
+		}
+	}
 	cout << "90 deg rotation\n";
-	for(auto &&row : matrix){
+	for(auto &&row : ans){
 		for(auto &&x : row){
 			cout << x << " " ;
 		}
