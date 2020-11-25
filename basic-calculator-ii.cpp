c4a05fb24efe611e2fba1a7dd75c1423a7748795
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2020-11-25 20:43:23 +0530

    working commit - Created using LeetHub

---
+++ basic-calculator-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+// Function to find precedence of 
+// operators.
+int precedence(char op){
+    if(op == '+'||op == '-')
+    return 1;
+    if(op == '*'||op == '/')
+    return 2;
+    return 0;
+}
+ 
+// Function to perform arithmetic operations.
+int applyOp(int a, int b, char op){
+    switch(op){
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+    }
+    return -1;
+}
+    
+    int calculate(string tokens) {
+         int i;
+     
+    // stack to store integer values.
+    stack <int> values;
+     
+    // stack to store operators.
+    stack <char> ops;
+     
+    for(i = 0; i < tokens.length(); i++){
+         
+        // Current token is a whitespace,
